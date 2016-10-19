858257523cf0aed37c0901f1ac66fc7c27edf958
CrazyRundong <lrd.001@163.com>
2016-10-20 02:10:56 +0800

    this game can run without bug now.... but code is still dirty

--- 2048_main.py
+++ 2048_main.py
@@ -7,19 +7,30 @@ from enum import Enum
 
 BOARD_WIDTH = 4
 BOARD_HEIGHT = 4
+BOARD_OUTER_LINE_WIDTH = 4
 BLOCK_SIZE = 100
 MARGIN_SIZE = 20
-TITLE_SIZE = 64
+TITLE_SIZE = 72
+FONT_SIZE = 64
+RESULT_SIZE = 50
 WINDOW_WIDTH = 640
 WINDOW_HEIGHT = 640
 MAX_SCORE = 2048
 INIT_SCORE = 2
 FPS = 30
 
+# check window size
+assert (BOARD_WIDTH * (BLOCK_SIZE + MARGIN_SIZE) < WINDOW_WIDTH) and \
+       (BOARD_HEIGHT * (BLOCK_SIZE + MARGIN_SIZE) + TITLE_SIZE < WINDOW_HEIGHT), \
+       'window_size is much too small!'
 X_MARGIN = int((WINDOW_WIDTH - (BOARD_WIDTH * (BLOCK_SIZE + MARGIN_SIZE))) / 2)
-Y_MARGIN = int((WINDOW_HEIGHT - TITLE_SIZE - (BOARD_HEIGHT * (BLOCK_SIZE + MARGIN_SIZE))) / 2)
+Y_MARGIN = int((WINDOW_HEIGHT - (BOARD_HEIGHT * (BLOCK_SIZE + MARGIN_SIZE))) / 2 + TITLE_SIZE / 2)
 TITLE_CENTER = (int(WINDOW_WIDTH / 2), int(Y_MARGIN / 2))
 
+# Set Result
+class Result(Enum):
+    Win = 1
+    Lost = 0
 
 # Set Color
 class Color(Enum):
@@ -37,9 +48,12 @@ class Color(Enum):
     Block512 = (236, 176, 77)
     Block1024 = (235, 148, 55)
     Block2048 = (234, 120, 33)
+    TextLight = (255, 244, 234)
+    TextDark = (119, 110, 101)
+
 
 BACKGROUND_COLOR = Color.Block0.value
-TEXT_COLOR = Color.Block2048.value
+TEXT_COLOR = Color.TextDark.value
 COLOR_SWITCHER = {
     0: Color.Block0.value,
     2: Color.Block2.value,
@@ -63,13 +77,14 @@ class Direction(Enum):
     Left = 2
     Right = 3
 
+
 # idx_boundary, idx_slide_start, idx_step, attr_name (col/line), attr_name (elem), range of another attr
 SLIDE_SWITCHER = {
-            Direction.Up: [0, BOARD_HEIGHT, 1, 'coordinate_x', 'coordinate_y', BOARD_WIDTH],
-            Direction.Down: [BOARD_HEIGHT - 1, -1, -1, 'coordinate_x', 'coordinate_y', BOARD_WIDTH],
-            Direction.Left: [0, BOARD_WIDTH, 1, 'coordinate_y', 'coordinate_x', BOARD_HEIGHT],
-            Direction.Right: [BOARD_WIDTH - 1, -1, -1, 'coordinate_y', 'coordinate_x', BOARD_HEIGHT],
-        }
+    Direction.Up: [0, BOARD_HEIGHT, 1, 'coordinate_x', 'coordinate_y', BOARD_WIDTH],
+    Direction.Down: [BOARD_HEIGHT - 1, -1, -1, 'coordinate_x', 'coordinate_y', BOARD_WIDTH],
+    Direction.Left: [0, BOARD_WIDTH, 1, 'coordinate_y', 'coordinate_x', BOARD_HEIGHT],
+    Direction.Right: [BOARD_WIDTH - 1, -1, -1, 'coordinate_y', 'coordinate_x', BOARD_HEIGHT],
+}
 
 
 class Block:
@@ -99,24 +114,28 @@ class Board:
                               getattr(block, SLIDE_SWITCHER[direction][3]) == line_col_idx]
             # search: [idx_boundary -> idx_move_start) by idx_step
             current_blocks.sort(key=lambda row: row[1], reverse=False if (SLIDE_SWITCHER[direction][2] == 1) else True)
-            previous_idx = 0
+            previous_idx = -1
             for block in current_blocks:
                 if block[1] == SLIDE_SWITCHER[direction][0]:  # element on boundary
                     setattr(block[0], 'slide_enable', False)
-                    setattr(block[0], 'next_'+SLIDE_SWITCHER[direction][3], line_col_idx)
-                    setattr(block[0], 'next_'+SLIDE_SWITCHER[direction][4], SLIDE_SWITCHER[direction][0])
+                    setattr(block[0], 'next_' + SLIDE_SWITCHER[direction][3], line_col_idx)
+                    setattr(block[0], 'next_' + SLIDE_SWITCHER[direction][4], SLIDE_SWITCHER[direction][0])
                 else:  # not boundary element
                     if (block[1] - SLIDE_SWITCHER[direction][2] not in [row[1] for row in current_blocks]) or \
                             (getattr(current_blocks[previous_idx][0], 'slide_enable') is True):
                         setattr(block[0], 'slide_enable', True)
-                        setattr(block[0], 'next_'+SLIDE_SWITCHER[direction][3], line_col_idx)
+                        setattr(block[0], 'next_' + SLIDE_SWITCHER[direction][3], line_col_idx)
                         # calc next coordinate
-                        if previous_idx == 0:
+                        if previous_idx == -1:
                             setattr(block[0], 'next_' + SLIDE_SWITCHER[direction][4], SLIDE_SWITCHER[direction][0])
                         else:
                             setattr(block[0], 'next_' + SLIDE_SWITCHER[direction][4],
                                     getattr(current_blocks[previous_idx][0],
                                             'next_' + SLIDE_SWITCHER[direction][4]) + SLIDE_SWITCHER[direction][2])
+                assert ((0 <= block[0].next_coordinate_x < BOARD_WIDTH) and
+                        (0 <= block[0].next_coordinate_y < BOARD_HEIGHT)), \
+                    'invalid next move: ' + str((block[0].next_coordinate_x, block[0].next_coordinate_y)) + \
+                    ' while current block is ' + str((block[0].coordinate_x, block[0].coordinate_y)) + '.'
                 previous_idx += 1
 
     def slide_block(self):
@@ -136,7 +155,8 @@ class Board:
                 continue
             else:
                 # merge: (idx_move_start -> idx_boundary] by idx_step
-                current_blocks.sort(key=lambda row: row[1], reverse=False if (SLIDE_SWITCHER[direction][2] == -1) else True)
+                current_blocks.sort(key=lambda row: row[1],
+                                    reverse=False if (SLIDE_SWITCHER[direction][2] == -1) else True)
                 next_idx = 1
                 for block in current_blocks:
                     if next_idx >= len(current_blocks):
@@ -148,15 +168,16 @@ class Board:
                     next_idx += 1
 
     def generate_block(self):
+        if len(self.blocks) >= (BOARD_WIDTH * BOARD_HEIGHT):
+            return
         all_position = []
         for x in range(BOARD_WIDTH):
             for y in range(BOARD_HEIGHT):
                 all_position.append((x, y))
         for block in self.blocks:
             (x, y) = (block.coordinate_x, block.coordinate_y)
-            # TODO(Rundong) some bug in this remove:
-            if (x, y) in all_position:
-                all_position.remove((x, y))
+            assert ((x, y) in all_position), 'contain block(s) with invalid location ' + str((x, y))
+            all_position.remove((x, y))
         random.shuffle(all_position)
         position_insert = all_position.pop()
         self.blocks.append(Block(position_insert[0], position_insert[1]))
@@ -167,7 +188,7 @@ class Board:
         self.slide_block()
         self.merge_block(direction)
         self.generate_block()
-        
+
     def get_max_score(self):
         return max([block.score for block in self.blocks])
 
@@ -176,21 +197,20 @@ class Board:
 
 
 def main():
-    global FPS_CLOCK, DISPLAY_SURF, FONT_OBJ
+    global FPS_CLOCK, DISPLAY_SURF, FONT_OBJ, TITLE_OBJ, BLOCK_BOARD
     pygame.init()
     FPS_CLOCK = pygame.time.Clock()
     DISPLAY_SURF = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-    block_board = pygame.Rect(X_MARGIN, Y_MARGIN,
-                              (BLOCK_SIZE + MARGIN_SIZE) * BOARD_WIDTH, (BLOCK_SIZE + MARGIN_SIZE) * BOARD_HEIGHT)
+    BLOCK_BOARD = pygame.Rect(X_MARGIN - BOARD_OUTER_LINE_WIDTH, Y_MARGIN - BOARD_OUTER_LINE_WIDTH,
+                              (BLOCK_SIZE + MARGIN_SIZE) * BOARD_WIDTH + BOARD_OUTER_LINE_WIDTH * 2 - MARGIN_SIZE,
+                              (BLOCK_SIZE + MARGIN_SIZE) * BOARD_HEIGHT + BOARD_OUTER_LINE_WIDTH * 2 - MARGIN_SIZE)
 
     # Set Font
-    FONT_SIZE = 32
     FONT_OBJ = pygame.font.Font('resource/SourceSansPro-Regular.ttf', FONT_SIZE)
+    TITLE_OBJ = pygame.font.Font('resource/SourceSansPro-Regular.ttf', TITLE_SIZE)
 
     main_board = Board()
-    slide_direction = Direction.Left
     title_text = 'Your Score: '
-    current_score = main_board.get_max_score()
 
     pygame.display.set_caption('2048')
     DISPLAY_SURF.fill(BACKGROUND_COLOR)
@@ -198,7 +218,7 @@ def main():
     # Main game loop
     while True:
         DISPLAY_SURF.fill(BACKGROUND_COLOR)
-        pygame.draw.rect(DISPLAY_SURF, Color.DeepOrange.value, block_board, 4)
+        pygame.draw.rect(DISPLAY_SURF, Color.DeepOrange.value, BLOCK_BOARD, BOARD_OUTER_LINE_WIDTH)
 
         for event in pygame.event.get():  # event handling loop
             if event.type == QUIT or (event.type == KEYUP and event.key == K_ESCAPE):
@@ -221,13 +241,18 @@ def main():
         current_score = main_board.get_max_score()
         draw_blocks(main_board)
 
-        if current_score < 2048:
+        if len(main_board.blocks) >= (BOARD_WIDTH * BOARD_HEIGHT):
+            title = 'Pity, you LOST!\nPress "Esc" to QUIT\nor any other key to RESTART.'
+            handle_win_or_lost(Result.Lost, title)
+            main_board = Board()
+        elif current_score < 2048:
             title = title_text + str(current_score)
+            draw_title(title)
         else:
-            title = 'Congratulations, you WIN!'
-        draw_title(title)
+            title = 'Congratulations, you WIN!\nPress "Esc" to QUIT\nor any other key to RESTART.'
+            handle_win_or_lost(Result.Win, title)
+            main_board = Board()
 
-        # Redraw the screen and wait a clock tick.
         pygame.display.update()
         FPS_CLOCK.tick(FPS)
 
@@ -236,16 +261,17 @@ def draw_blocks(board_in):
     assert isinstance(board_in, Board), 'function "draw_blocks": accepts "Board" class argument only.'
     for block in board_in.blocks:
         left, top = block_position_to_pixel(block.coordinate_x, block.coordinate_y)
-        pygame.draw.rect(DISPLAY_SURF, COLOR_SWITCHER[block.score], (left, top, BLOCK_SIZE, BLOCK_SIZE))
+        block_rect_obj = pygame.Rect(left, top, BLOCK_SIZE, BLOCK_SIZE)
+        pygame.draw.rect(DISPLAY_SURF, COLOR_SWITCHER[block.score], block_rect_obj)
         text_surface_obj = FONT_OBJ.render(str(block.score), True, TEXT_COLOR)
         text_rect_obj = text_surface_obj.get_rect()
-        text_rect_obj.topleft = (left, top)
+        text_rect_obj.center = block_rect_obj.center
         DISPLAY_SURF.blit(text_surface_obj, text_rect_obj)
 
 
 def block_position_to_pixel(x, y):
-    # TODO(Rundong) really strange BUG here if you don't comment below
-    # assert (0 <= x) and (0 <= y), 'function "block_position_to_pixel": position (x, y) must be positive integers.'
+    assert (0 <= x < BOARD_WIDTH) and (0 <= y < BOARD_HEIGHT), \
+        'function "block_position_to_pixel": invalid position ' + str((x, y)) + ', which must be in [0, board_size).'
     pixel_left = X_MARGIN + (BLOCK_SIZE + MARGIN_SIZE) * x
     pixel_top = Y_MARGIN + (BLOCK_SIZE + MARGIN_SIZE) * y
 
@@ -254,10 +280,43 @@ def block_position_to_pixel(x, y):
 
 def draw_title(title):
     assert isinstance(title, str), 'function "draw_title": title must be string.'
-    text_surface_obj = FONT_OBJ.render(title, True, TEXT_COLOR)
+    text_surface_obj = TITLE_OBJ.render(title, True, TEXT_COLOR)
     text_rect_obj = text_surface_obj.get_rect()
     text_rect_obj.center = TITLE_CENTER
     DISPLAY_SURF.blit(text_surface_obj, text_rect_obj)
+    
+
+def handle_win_or_lost(result, title):
+    assert isinstance(result, Result), 'function "handle_win_or_lost": result must be "Result" class.'
+    DISPLAY_SURF.fill(BACKGROUND_COLOR)
+    text_y = Y_MARGIN + int(TITLE_SIZE / 2)
+    result_font = pygame.font.Font('resource/SourceSansPro-Regular.ttf', RESULT_SIZE)
+
+    tokens = title.split('\n')
+    text_surface_obj = TITLE_OBJ.render(tokens[0], True, TEXT_COLOR, BACKGROUND_COLOR)
+    text_rect_obj = text_surface_obj.get_rect()
+    text_rect_obj.top = text_y
+    text_rect_obj.centerx = int(WINDOW_WIDTH / 2)
+    DISPLAY_SURF.blit(text_surface_obj, text_rect_obj)
+    text_y += RESULT_SIZE
+
+    for text in tokens[1:]:
+        text_y += int(RESULT_SIZE * 1.5)
+        text_surface_obj = result_font.render(text, True, TEXT_COLOR, BACKGROUND_COLOR)
+        text_rect_obj = text_surface_obj.get_rect()
+        text_rect_obj.top = text_y
+        text_rect_obj.centerx = int(WINDOW_WIDTH / 2)
+        DISPLAY_SURF.blit(text_surface_obj, text_rect_obj)
+
+    while True:
+        for event in pygame.event.get():  # event handling loop
+            if event.type == QUIT or (event.type == KEYUP and event.key == K_ESCAPE):
+                pygame.quit()
+                sys.exit()
+            elif event.type == KEYUP:
+                return
+        pygame.display.update()
+        FPS_CLOCK.tick(FPS)
 
 
 if __name__ == '__main__':
--- README.md
+++ README.md
@@ -2,12 +2,20 @@
 
 这个项目是对流行游戏 `2048` 的简单模仿，游戏使用 ` Python3 ` 和 `PyGame` 开发。
 
-**目前正在开发中（现在这个游戏有非常奇妙的 BUG，简直魔性得不行）**
+**目前已经初步完成，但是代码比较 *dirty***
 
 ## Install
 
 - 下载安装 [`Python3`](https://www.python.org/downloads/) （`Python2` 应该也能运行） 和 [`PyGame`](http://www.pygame.org/download.shtml)。如果你使用的是 64 位系统，可以选择 U.C.Irvine [Dr.Christoph Gohlke](http://www.lfd.uci.edu/~gohlke/pythonlibs/#pygame) 编译的 64bit 版 PyGame;
 - 在根目录下执行 `python ./2048_main.py`。
+
+## Screen shots
+
+![running](resource/img/running.png) | ![fail](resource/img/fail.png)
+:---:|:---:
+program running | failure screen
+
+*别问我为什么没有 win screen 截图，手残党表示打不到 2048* T_T
 
 ## License
 
@@ -20,17 +28,25 @@
 
 This's a simple clone of popular game `2048` via `Python3` and `PyGame`.
 
-**This project is under develpoing. If you try to run it now, you'll enjoy yourself with some amazing bugs :)**
+**This project reaches its goal preliminarily, while the code is still kind of *dirty*.**
 
 ## Install
 
 - Download and install [`Python3`](https://www.python.org/downloads/) (`Python2` should be fine, too) and [`PyGame`](http://www.pygame.org/download.shtml). If you use 64bit system, you may try the 64bit variant of [`PyGame`](http://www.lfd.uci.edu/~gohlke/pythonlibs/#pygame) complied by Dr.Christoph Gohlke, U.C.Irvine;
 - Open you bash / CMD in the root directory and run `python ./2048_main.py`.
 
+## Screen shots
+
+![running](resource/img/running.png) | ![fail](resource/img/fail.png)
+:---:|:---:
+program running | failure screen
+
+*Please don't ask for a screen shot of win screen, I can't reach 2048...* T_T
+
 ## License
 
-- You can use / destribute / modify this code as your wish without my permission;
-- You can use this code without any `copyright` or `copyleft` attached;
+- You may use / distribute / modify this code as your wish without my permission;
+- You may use this code without any `copyright` or `copyleft` attached;
 - This code's just for individual usage only, any non-individual (**include educational**) usage is forbidden;
 - Iterative updates or bug fixing are NOT Guaranteed.
 
